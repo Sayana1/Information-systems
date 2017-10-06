2acb336f4f19a96dcd7ac1916a450fb19ae17caf
Sayana1 <sayanasendazhy@mail.ru>
2017-10-06 20:44:18 +0300

    task 2

--- Main.java
+++ Main.java
@@ -1,55 +1,41 @@
 package com.company;
 
-import java.util.Scanner;
-
-
+import  java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        System.out.println("Enter a hexadecimal string ");
+        public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+            System.out.println("enter a string" );
 
-        String s = scan.next();
-        s = s.toUpperCase();
+            Scanner scan = new Scanner(System.in);
+            String s = scan.next();
 
-        for(int i = 0; i < s.length(); i++)
-        {
-            char c = s.charAt(i);
-            if(c > 'F')
-            {
-                System.out.println("error");
-                return;
-            }
+            String encode = rot13(s);
+            String decode = rot13(encode);
+
+            System.out.println("Enoded string:" );
+            System.out.println(encode);
+
+            System.out.println("Decoded string:" );
+            System.out.println(decode);
+
         }
-        int a = _16to10(s);
 
-        System.out.println(a);
-
-    }
-
-
-
-
-    public static int _16to10(String s)
+    public static String rot13(String s)
     {
-        String hex = "0123456789ABCDEF";
-
-        int s_len = s.length();
-        int result = 0;
-
-
-        for(int i = 0; i < s_len; i++)
-        {
+        int s_length = s.length();
+        char result [] = new char [s_length];
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int a = hex.indexOf(c);
-
-            result = result + (a * (int)Math.pow(16, (s_len-i-1)));
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            result[i] = c;
         }
-
-        return result;
-
+        String r = String.valueOf(result);
+        return r;
     }
 }
