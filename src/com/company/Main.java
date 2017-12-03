d4c2154915eae01ecbc10e9f940d26870229c287
Sayana1 <sayanasendazhy@mail.ru>
2017-12-03 19:01:19 +0300

    task 1 redone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,82 @@ public class Main {
         System.out.println("enter n m");
         int n = scan.nextInt();
         int m = scan.nextInt();
-        System.out.println(gcd(n, m));
+        if(n < 0 && m > 0) {
+            System.out.println("gcd %");
+            System.out.println(gcd1(-n, m));
+            System.out.println("gcdFloorMod");
+            System.out.println(gcdFloorMod(-n, m));
+            System.out.println("rem");
+            System.out.println(gcdRem(-n, m));
+        }
+        else if(n > 0 && m < 0)
+        {
+            System.out.println("gcd %");
+            System.out.println(gcd1(n, -m));
+            System.out.println("gcdFloorMod");
+            System.out.println(gcdFloorMod(n, -m));
+            System.out.println("rem");
+            System.out.println(gcdRem(n, -m));
+        }
+        else if (n < 0 && m < 0)
+        {
+
+        }
+        else{
+            System.out.println("gcd %");
+            System.out.println(gcd1(-n, -m));
+            System.out.println("gcdFloorMod");
+            System.out.println(gcdFloorMod(-n, -m));
+            System.out.println("rem");
+            System.out.println(gcdRem(-n, -m));
+        }
     }
 
-    public static int gcd(int n, int m) {
-        while (m != 0) {
-            int tmp = n % m;
-            n = m;
-            m = tmp;
+    public static int gcd1(int n, int m) {
+        while (n != 0 && m != 0) {
+            if (Math.abs(n) > Math.abs(m)) {
+                n = n % m;
+            }
+            else {
+                m = m % n;
+            }
+        }
+        return n | m;
+    }
+    
+     static int gcdFloorMod(int n, int m) {
+
+        while (n != 0 && m != 0) {
+            if (Math.abs(n) > Math.abs(m)) {
+                n = Math.floorMod(n, m);
+            } else {
+                m = Math.floorMod(m, n);
+            }
+        }
+        return n | m;
+    }
+    
+     public static int rem(int n, int m) {
+        if (m < 0) {
+            return rem(n, -m);
+        }
+        if (n < 0) {
+            return rem(-n, m);
+        }
+        while (n >= m) {
+            n = n - m;
         }
         return n;
+    }
+    
+     static int gcdRem(int n, int m) {
+        while (n != 0 && m != 0) {
+            if (Math.abs(n) > Math.abs(m)) {
+                n = rem(n, m);
+            } else {
+                m = rem(m, n);
+            }
+        }
+        return n | m;
     }
 }
